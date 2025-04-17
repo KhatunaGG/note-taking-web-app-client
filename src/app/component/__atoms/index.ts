a9eb73d56f181b153aa5aa978a5ef020b74d1293
Gagloshvili <KGagloshvili@oriflame.com>
2025-04-17 11:59:31 +0400

    added: sign-in-markup

--- src/app/component/__atoms/index.ts
+++ src/app/component/__atoms/index.ts
@@ -1,1 +1,4 @@
 export { default as InfoCircle } from "./infoCircle/InfoCircle";
+export { default as GoogleText } from "./googleText/GoogleText";
+export { default as GoogleIcon } from "./googleIcon/GoogleIcon";
+export { default as Eye } from "./eye/Eye";
--- src/app/component/__molecules/emailInput/EmailInput.tsx
+++ src/app/component/__molecules/emailInput/EmailInput.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Eye } from "../../__atoms";
 
 const EmailInput = () => {
   return (
